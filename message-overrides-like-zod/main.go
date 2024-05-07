e538ff8145f19c98e2cb580a0620137800b2ad7c
Kubernetes Release Robot <k8s-release-robot@users.noreply.github.com>
2024-05-07 23:22:42 +0100

    zod like error message, now I have to refactor

--- message-overrides-like-zod/main.go
+++ message-overrides-like-zod/main.go
@@ -15,11 +15,12 @@ import (
 )
 
 // User contains user information
+
 type User struct {
 	FirstName      string     `json:"first_name" validate:"required"`
 	LastName       string     `json:"last_name" validate:"required"`
-	Age            uint8      `validate:"gte=0,lte=130"`
-	Email          string     `validate:"required,email"`
+	Age            uint8      `json:"age" validate:"gte=0,lte=130"`
+	Email          string     `json:"email" validate:"required,email"`
 	FavouriteColor string     `validate:"hexcolor|rgb|rgba"`
 	Addresses      []*Address `validate:"required,dive,required"` // a person can have a home and cottage...
 }
@@ -51,7 +52,7 @@ func main() {
 	transFr.Add("{{first_name}}", "Prénom", false)
 	transEn.Add("{{last_name}}", "Last Name", false)
 	transFr.Add("{{last_name}}", "Nom de famille", false)
-	transEn.Add("{{email}}", "Last Name", false)
+	// transEn.Add("{{email}}", "Last Name", false)
 	transEn.Add("{{id}}", "Last Name", false)
 	transEn.Add("{{customerId}}", "Last Name", false)
 	validate = validator.New()
@@ -64,11 +65,13 @@ func main() {
 		}
 		println(" this is how name looks like: ", name)
 
-		return "{{" + name + "}}"
+		return name // what are these brackets used for
+		// return "{{" + name + "}}" // what are these brackets used for
 	})
 
 	en_translations.RegisterDefaultTranslations(validate, transEn)
 	fr_translations.RegisterDefaultTranslations(validate, transFr)
+	translateOverrideHereNew(transEn) // yep you can specify your own in whatever locale you want!
 
 	// build 'User' info, normally posted data etc...
 	address := &Address{
@@ -99,7 +102,7 @@ func main() {
 			return
 		}
 
-		errMsg := make(map[string]string)
+		// errMsg := make(map[string]string)
 
 		for _, err := range err.(validator.ValidationErrors) {
 
@@ -118,16 +121,60 @@ func main() {
 				fmt.Println()
 			*/
 			jsonKey := err.Field()
-			fieldName, _ := transFr.T(jsonKey)
-			message := strings.Replace(err.Translate(transFr), jsonKey, fieldName, -1)
-			jsonKey = jsonKey[2 : len(jsonKey)-2]
-			errMsg[jsonKey] = message
-			fmt.Println(jsonKey, ":", errMsg[jsonKey])
+			// fieldName, _ := transFr.T(jsonKey)
+			// message := strings.Replace(err.Translate(transFr), jsonKey, fieldName, -1)
+			fmt.Println("jsonKey here bro: ", jsonKey)
+
+			// jsonKey = jsonKey[2 : len(jsonKey)-2]
+			// errMsg[jsonKey] = message
+			// fmt.Println(jsonKey, ":", errMsg[jsonKey])
+			fmt.Println(jsonKey + " must be defined")
 		}
 
 		// from here you can create your own error messages in whatever language you wish
 		return
 	}
 
+	// TODO:TODO: since i can get the tag now, it is not time to make sure it is required
 	// save user to database
+}
+
+func translateOverrideHereNew(trans ut.Translator) {
+	// TODO:TODO: this works but I need to use the json tag and not the struct field
+	// TODO:TODO: this is what i am looking for
+	// source:https://github.com/syssam/go-playground-sample/blob/master/main.go
+	validate.RegisterTranslation("required", trans, func(ut ut.Translator) error {
+		return ut.Add("required", "{0} must have a value!", true) // see universal-translator for details
+	}, func(ut ut.Translator, fe validator.FieldError) string {
+		// t, _ := ut.T("required", fe.Field())
+		fmt.Println("FieldError validator fe.Field here: 92", fe.Field())
+		fmt.Println("FieldError validator fe.Tag here: 90", fe.Tag())
+		fmt.Println("FieldError validator fe.ActualTag here: 90", fe.ActualTag())
+		fmt.Println("FieldError validator fe.StructField here: 90", fe.StructField())
+		fmt.Println("FieldError validator fe.Type here: 90", fe.Type())
+		fmt.Println("FieldError validator fe.Kind here: 90", fe.Kind())
+
+		// t, _ := ut.T("required", fe.Namespace())
+		t, _ := ut.T("required", fe.Field())
+
+		return t
+	})
+
+	type User struct {
+		Username string `json:"dexter" validate:"required"`
+	}
+
+	var user User
+
+	err := validate.Struct(user)
+	if err != nil {
+
+		errs := err.(validator.ValidationErrors)
+
+		for _, e := range errs {
+			// can translate each error one at a time.
+			// TODO:TODO: this is where the error message matters
+			fmt.Println("some translations things here: 223", e.Translate(trans))
+		}
+	}
 }
